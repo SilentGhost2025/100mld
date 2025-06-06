457c51da46237b831fc125f8e15d187eb203de29
salvation <abolarinsalvation2005@gmail.com>
2025-06-06 08:26:01 -0700

    bug fixed please review

--- program.py
+++ program.py
@@ -5,3 +5,5 @@ print("Your name is " + name + " and your age is " + age )
 
 print"Thgis is my newly updated line"
 
+print "This code was edited while trying to fix a bug under the bug_fix branch"
+
